172625ba0692d4ebb7561153b857f611f1c5785e
FaizAlfaRizqy <muhammadumarfaiz18@gmail.com>
2025-12-15 21:34:45 +0700

    terahir bangg

--- auth/login.php
+++ auth/login.php
@@ -113,7 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 
                 <div class="button-group">
                     <button type="submit" class="btn-confirm">CONFIRM</button>
-                    <a href="../index.php" class="btn-back">BACK</a>
+                    <a href="../register.php" class="btn-back">REGISTER</a>
                 </div>
             </form>
             
